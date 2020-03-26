2dbc3be0e653f799ebe669b6ed056985e2f21526
Fernando Mendoza Escobar <ferchome0@gmail.com>
2020-03-26 15:57:19 -0400

    configurando icono

--- src/public/worker.js
+++ src/public/worker.js
@@ -1,15 +1,15 @@
-console.log('Service Worker')
+console.log('Service Worker');
 
 /**
  * Función encargada de escuhar la notificación
  */
 self.addEventListener('push', e => {
-  const data = e.data.json()
-  console.log(data)
+  const data = e.data.json();
+  console.log(data);
   self.registration.showNotification(data.title, {
     body: data.message,
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Archlinux-icon-crystal-64.svg/1200px-Archlinux-icon-crystal-64.svg.png'
-  })
-  console.log('Notification received')
-}) // datos recibidos
+    icon:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Archlinux-icon-crystal-64.svg/1200px-Archlinux-icon-crystal-64.svg.png',
+  });
+  console.log('Notification received');
+}); // datos recibidos
